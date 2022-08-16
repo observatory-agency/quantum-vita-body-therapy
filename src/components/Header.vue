5f32c2b3fe0e96653f4706b125d9b8080730565c
Pete Lloyd <plloyd11@gmail.com>
2022-08-16 17:37:29 -0400

    updates

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -112,7 +112,7 @@
                 </nav>
                 <a
                   href="/contact"
-                  class="w-full py-2 text-lg text-gray-100 transition-all transform border border-transparent rounded-md lg:inline-block bg-primaryColor hover:bg-primaryColorDark"
+                  class="block w-full py-2 pl-2 text-lg text-gray-100 transition-all transform border border-transparent rounded-md lg:inline-block bg-primaryColor hover:bg-primaryColorDark"
                 >
                   Request an appointment
                 </a>
